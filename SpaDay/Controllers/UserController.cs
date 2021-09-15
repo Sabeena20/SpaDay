bd0d30028852ea9a86c0fe774d2324e34d4f4a6d
Sabeena <sabeenarayyan13@gmail.com>
2021-09-15 12:56:00 -0500

    added some code

---
+++ SpaDay/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpaDay.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Route("/user/add/")]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        public IActionResult SubmitAddUserForm(User newUser, string verify)
+        {
+            // add form submission handling code here
+        }
+    }
+}
